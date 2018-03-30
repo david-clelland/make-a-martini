c2063f58fb78b7f32ac4e0354c470f08f6fc4650
David <david@getoperatr.com>
2018-03-30 18:51:55 -0400

    added functions and beginning of game logic

--- script.js
+++ script.js
@@ -14,18 +14,54 @@ $(document).ready(function() {
   const $welcomeMessage = $('#welcome-message');
   const playerName = $('#name-input');
   const player = $('#player');
-  const $answer = [];
   const $pantryBox = $('.pantry-box');
-  const $cardAnswer = [1, 2, 3];
+  const $pantryOne = $('#pantry-box-one ');
+  const $pantryTwo = $('#pantry-box-two ');
+  const $pantryThree = $('#pantry-box-three ');
+  const $pantryFour = $('#pantry-box-four ');
+  const $pantryFive = $('#pantry-box-five ');
+  const $pantrySix = $('#pantry-box-six ');
+  const $pantrySeven = $('#pantry-box-seven ');
+  const $pantryEight = $('#pantry-box-eight ');
+  const $pantryNine = $('#pantry-box-nine ');
+  const $pantryTen = $('#pantry-box-ten ');
+  const $pantryEleven = $('#pantry-box-eleven ');
+  const $pantryTwelve = $('#pantry-box-twelve ');
+  const $pantryThirteen = $('#pantry-box-thirteen ');
+  const $pantryFourteen = $('#pantry-box-fourteen ');
+  const $pantryFifteen = $('#pantry-box-fifteen ');
+  const $answer = [];
+  const $cardAnswer = [];
+
 // ======================================================
 // resuable functions
-// tried storing some reusable functions but kept breaking my code
+// ======================================================
+  function pantryClick(box){
+       box.on('click', function() {
+       $answer.push(box)
+       console.log($answer);
+       box.off();
+    });
+}
+
+function isEqual(value, other){
+  for(let i=0; i< $cardAnswer.length; i++ ){
+    if($answer[i] === $cardAnswer[i]){
+    console.log('correct');
+  }else if($answer[i] !== $cardAnswer[i]){
+    console.log('you were wrong');
+  }
+  }
+};
+
+
 // =======================================================
 // Hidden pages and elements
 // =======================================================
-  $gameBoard.hide();
-  $pantryBoard.hide();
+  // $gameBoard.hide();
+  // $pantryBoard.hide();
   $cardBack.hide();
+
 // =======================================================
 // player name and game start
 // =======================================================
@@ -37,62 +73,76 @@ $(document).ready(function() {
     });
       $welcomeMessage.append(`Hello ${playerName.val()} you will have 5 seconds to remember the ingredients, click the cocktail when you're ready!`);
   });
+
 // =======================================================
 // click event (toggle) on CardFront to change the image to cardBack and trigger countdown
 // =======================================================
-$("#card").click(function(){
+$card.on('click', function(){
   $cardFront.toggle();
   $cardBack.toggle();
+  $card.off();
+
 // =======================================================
 // timer for gameBoard
 // =======================================================
-  let counter = 5;
+  let counter = 2;
   let interval = setInterval(function() {
     counter--;
     $timer.html(counter);
     if (counter === 0) {
       $timer.html('Time is up!');
       clearInterval(interval);
+
 // =======================================================
 // switches to the pantryBoard when the timer runs out
 // =======================================================
       $gameBoard.fadeOut('slow/1000/slow', function() {
       });
       $pantryBoard.fadeIn('slow/1000/slow', function() {
+
 // ======================================================
 // timer for pantryBoard
 // ======================================================
-          let counter = 10;
+          let counter = 2;
           let interval = setInterval(function() {
           counter--;
           $timerTwo.html(counter);
           if (counter === 0) {
+          $pantryBox.off();
           $timerTwo.html('Time is up!');
           clearInterval(interval);
+          isEqual($answer, $cardAnswer);
     }
 }, 1000);
+
+
 // =======================================================
 // Click event on pantry boxes
 // =======================================================
-          $pantryBox.click(function(){
-          $answer.push($pantryBox)
-          $pantryBox.html()
-          console.log($answer)
-          });
-      });
+   pantryClick($pantryOne);
+   pantryClick($pantryTwo);
+   pantryClick($pantryThree);
+   pantryClick($pantryFour);
+   pantryClick($pantryFive);
+   pantryClick($pantrySix);
+   pantryClick($pantrySeven);
+   pantryClick($pantryEight);
+   pantryClick($pantryNine);
+   pantryClick($pantryTen);
+   pantryClick($pantryEleven);
+   pantryClick($pantryTwelve);
+   pantryClick($pantryThirteen);
+   pantryClick($pantryFourteen);
+   pantryClick($pantryFifteen);
+  });
+
 // =======================================================
     }
 }, 1000);
 });
+
 // =======================================================
 // Game Logic
 // =======================================================
-// if ($answer === $cardAnswer){
-//   console.log("correct!");
-// }else{
-//   console.log("You were wrong");
-// }
-console.log($cardAnswer)
-
 
 });
